d4d741606d0238bba6e3e19460384782fb089c77
Ayomide <osunmuyiwaa@gmail.com>
2022-10-13 16:16:47 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,6 @@
 
 int main(void)
 {
-	char alx[] = "Programming is like building a multilingual puzzle\n";
-	puts(alx);
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return 0;
 }
