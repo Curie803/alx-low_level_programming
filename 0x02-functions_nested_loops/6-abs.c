f4d332c8ac85b0d5f3332923808feda6101a49ab
Ayomide <osunmuyiwaa@gmail.com>
2022-10-18 16:07:04 -0700

    commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - return s the absolute value
+ * @value: value to be returned
+ * Return: 0 if successful
+ */
+
+int _abs(int value)
+{
+	if (value < 0)
+	{
+		return (-(value));
+	}
+	else
+	{
+		return (value);
+	}
+}
+
