4a26f8c3e5e380f49626905647919e68d10d95b6
Ayomide <osunmuyiwaa@gmail.com>
2022-10-21 16:31:35 -0700

    commit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,5 +9,9 @@ void print_most_numbers(void);
 int mul(int a, int b);
 void more_numbers(void);
 void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_traingle(int size);
+void print_number(int n);
 
 #endif
