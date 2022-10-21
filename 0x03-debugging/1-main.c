735837b0a95e16788130a0e12faf298314402ede
Ayomide <osunmuyiwaa@gmail.com>
2022-10-20 17:14:02 -0700

    commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+
 /**
- * main - cause infinite loop
+ * main - causes an infinite loop
  * Return: 0
  */
 
@@ -13,9 +14,11 @@ int main(void)
 	i = 0;
 
 	/*while (i < 10)*/
-	{
-		/*putchar(i);*/
-	}
+	/*{*/
+	/*putchar(i);*/
+	/*}*/
+
 	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
