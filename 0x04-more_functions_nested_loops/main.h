11c693dc147c2809144a1eeca5ab22280ccfc0a8
Ayomide <osunmuyiwaa@gmail.com>
2022-10-21 16:04:25 -0700

    commit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,5 +6,8 @@ int _isupper(int c);
 int _isdigit(int c);
 void print_numbers(void);
 void print_most_numbers(void);
+int mul(int a, int b);
+void more_numbers(void);
+void print_line(int n);
 
 #endif
